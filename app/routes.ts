8dba53c2fb6c2305d87ce43142380f248a7cd97b
deni rachmadi <rachmadideni@gmail.com>
2025-11-07 08:24:45 +0800

    add task route

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,11 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, prefix, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"),
+    ...prefix("tasks", [
+        layout("layouts/stack.tsx", [
+            index("routes/task/task.tsx"),
+            route("add", "routes/task/add.tsx")
+        ])
+    ]),
+] satisfies RouteConfig;
